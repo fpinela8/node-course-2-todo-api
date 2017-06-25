dd5c820cb2749391094e48c9fd020f4125813c8b
Fabio <fabio.pinela@gmail.com>
2017-06-25 17:35:31 +0100

    Adding DELETE tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -98,3 +98,40 @@ describe('GET /todos/:id', () => {
       .end(done);
   });
 });
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err){
+          return done(err);
+        }
+
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    request(app)
+        .delete('/todos/594fb2c2d718a70c250f890e')
+        .expect(404)
+        .end(done);
+  });
+
+  it('should return 404 if object id invalid', (done) => {
+    request(app)
+        .delete('/todos/123e')
+        .expect(404)
+        .end(done);
+  });
+});
